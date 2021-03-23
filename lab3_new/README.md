4f70d038b65beccc91e8eef91699ffc78ec15901
Tommy Giang <tommy.giang@sjsu.edu>
2021-03-22 17:47:10 -0700

    Finished the new Lab3
    Finished the new Lab3. Desired output in README.md and full output of experiment is in output.txt.

--- lab3_new/README.md
+++ lab3_new/README.md
@@ -11,6 +11,28 @@
 - False Positives (FP): we incorrectly predicted that they do have diabetes (a "Type I error")
 - False Negatives (FN): we incorrectly predicted that they don't have diabetes (a "Type II error")
 
+-----------------------------
+
+## Student Notes and Desired Output
+The full output of my experiment can be seen in output.txt. I ran 4 separate experiments aside from the baseline with various changes to which feature columns to use / include for training and testing the model, the value of the regularization parameter C in LogisticRegression, and the value of the test_size and random_state of sklearn train_test_split function.
+
+The desired output is shown below:
+| Experiement | Accuracy | Confusion Matrix | Comment |
+|-------------|----------|------------------|---------|
+| Baseline    | 0.6770833333333334 | [[114  16] [ 46  16]] | feature_cols = ['pregnant', 'insulin', 'bmi', 'age'] ; C = 1.0 ; test_size = 0.25 ; random_state = 0
+| Solution 1   | 0.7916666666666666 | [[115  15] [25 37]] |  feature_cols = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age'] ; C = 1.0 ; test_size = 0.25 ; random_state = 0 |
+| Solution 2   | 0.8246753246753247  | [[98  9] [18 29]] |  feature_cols = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age'] ; C = 1.0 ; test_size = 0.20 ; random_state = 0 |
+| Solution 3   | 0.7922077922077922  | [[95 12] [20 27]] |  feature_cols = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age'] ; C = 2.0 ; test_size = 0.20 ; random_state = 0 |
+| Solution 4   | 0.7857142857142857  | [[87 13] [20 34]] |  feature_cols = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age'] ; C = 1.0 ; test_size = 0.20 ; random_state = 5 |
+
+To explain, in Solution1, I decided to use all of the features to train the LogisticRegression model and test the test data. As we can see, the accuracy improved dramatically (up 12% from the baseline that only used pregnant, insulin, bmi, and age as features). We can see that the confusion matrix now has less false negatives (almost halved from baseline) and also more true positives (37 versus 16 of baseline). 
+
+In Solution2, I decided to change the test size from 0.25 (default) to 0.20 so that more samples can be used for training (but tradeoff is that there will be less data samples for testing). This improved our accuracy a bit (up 3 percent from Solution1). I still used all the features as I believe they are all important. As we can see from the confusion matrix, the number of false negatives is 18 and the number of false positives is just 9 (however, we need to take into consideration that the test size is now smaller, and there are only 107 negative and 47 positive samples).
+
+In Solution3, I decided to use Solution2 but instead of the regularization parameter of LogisticRegression set to 1.0, I used C=2.0 (more regularization, limits the "slope" of the model. Introduce a bit of bias so that the variance is lower). As we can see, this regularization added a bit too much bias as we can see, the accuracy decreased 3% from Solution2. The confusion matrix shows that there are now more false positives (12 vs 9 of solution2).
+
+In Solution4, I decided to use our best Solution2, but changed the random state of train_test_split to 5 (instead of 0). As we can see, this random state lowered the accuracy by 3% (to 78%). This shows that how we select samples in training and testing from the dataset can result in different results. This is why cross validation is important to confirming whether or not your model is the best.
+
 
 
 
--- lab3_new/lab3.py
+++ lab3_new/lab3.py
@@ -9,28 +9,27 @@ class DiabetesClassifier:
     def __init__(self) -> None:
         col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
         self.pima = pd.read_csv('diabetes.csv', header=0, names=col_names, usecols=col_names)
-        print(self.pima.head())
+        print(self.pima.head().to_markdown())
         self.X_test = None
         self.y_test = None
         
 
-    def define_feature(self):
-        feature_cols = ['pregnant', 'insulin', 'bmi', 'age']
+    def define_feature(self, feature_cols):
         X = self.pima[feature_cols]
         y = self.pima.label
         return X, y
     
-    def train(self):
+    def train(self, feature_cols, C, test_size, random_state):
         # split X and y into training and testing sets
-        X, y = self.define_feature()
-        X_train, self.X_test, y_train, self.y_test = train_test_split(X, y, random_state=0)
+        X, y = self.define_feature(feature_cols)
+        X_train, self.X_test, y_train, self.y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
         # train a logistic regression model on the training set
-        logreg = LogisticRegression()
+        logreg = LogisticRegression(C=C)
         logreg.fit(X_train, y_train)
         return logreg
     
-    def predict(self):
-        model = self.train()
+    def predict(self, feature_cols, C, test_size, random_state):
+        model = self.train(feature_cols, C, test_size, random_state)
         y_pred_class = model.predict(self.X_test)
         return y_pred_class
 
@@ -50,11 +49,100 @@ class DiabetesClassifier:
         return metrics.confusion_matrix(self.y_test, result)
     
 if __name__ == "__main__":
-    classifer = DiabetesClassifier()
-    result = classifer.predict()
+    print("Running Baseline training and prediction with LogisticRegression...\n")
+    print("feature_cols = ['pregnant', 'insulin', 'bmi', 'age'] ; C = 1.0 ; test_size = 0.25 ; random_state = 0\n")
+    feature_cols = ['pregnant', 'insulin', 'bmi', 'age']
+    C = 1.0
+    test_size = 0.25
+    random_state = 0
+    classifer0 = DiabetesClassifier()
+    result = classifer0.predict(feature_cols, C, test_size, random_state)
     print(f"Predicition={result}")
-    score = classifer.calculate_accuracy(result)
-    print(f"score={score}")
-    con_matrix = classifer.confusion_matrix(result)
-    print(f"confusion_matrix=${con_matrix}")
+    score0 = classifer0.calculate_accuracy(result)
+    print(f"score={score0}")
+    con_matrix0 = classifer0.confusion_matrix(result)
+    print(f"confusion_matrix=\n{con_matrix0}")
+
+    print("\nRunning Solution1 training and prediction with LogisticRegression...\n")
+    print("feature_cols = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age'] ; C = 1.0 ; test_size = 0.25 ; random_state = 0")
+    feature_cols = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age']
+    C = 1.0
+    test_size = 0.25
+    random_state = 0
+    classifer1 = DiabetesClassifier()
+    result = classifer1.predict(feature_cols, C, test_size, random_state)
+    print(f"Predicition={result}")
+    score1 = classifer1.calculate_accuracy(result)
+    print(f"score={score1}")
+    con_matrix1 = classifer1.confusion_matrix(result)
+    print(f"confusion_matrix=\n{con_matrix1}")
+
+    print("\nRunning Solution2 training and prediction with LogisticRegression...\n")
+    print("feature_cols = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age'] ; C = 1.0 ; test_size = 0.20 ; random_state = 0")
+    feature_cols = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age']
+    C = 1.0
+    test_size = 0.20
+    random_state = 0
+    classifer2 = DiabetesClassifier()
+    result = classifer2.predict(feature_cols, C, test_size, random_state)
+    print(f"Predicition={result}")
+    score2 = classifer2.calculate_accuracy(result)
+    print(f"score={score2}")
+    con_matrix2 = classifer2.confusion_matrix(result)
+    print(f"confusion_matrix=\n{con_matrix2}")
+
+    print("\nRunning Solution3 training and prediction with LogisticRegression...\n")
+    print("feature_cols = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age'] ; C = 2.0 ; test_size = 0.20 ; random_state = 0")
+    feature_cols = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age']
+    C = 2
+    test_size = 0.20
+    random_state = 0
+    classifer3 = DiabetesClassifier()
+    result = classifer3.predict(feature_cols, C, test_size, random_state)
+    print(f"Predicition={result}")
+    score3 = classifer3.calculate_accuracy(result)
+    print(f"score={score3}")
+    con_matrix3 = classifer3.confusion_matrix(result)
+    print(f"confusion_matrix=\n{con_matrix3}")
+
+    print("\nRunning Solution4 training and prediction with LogisticRegression...\n")
+    print("feature_cols = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age'] ; C = 1.0 ; test_size = 0.20 ; random_state = 5")
+    feature_cols = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age']
+    C = 1.0
+    test_size = 0.20
+    random_state = 5
+    classifer4 = DiabetesClassifier()
+    result = classifer4.predict(feature_cols, C, test_size, random_state)
+    print(f"Predicition={result}")
+    score4 = classifer4.calculate_accuracy(result)
+    print(f"score={score4}")
+    con_matrix4 = classifer4.confusion_matrix(result)
+    print(f"confusion_matrix=\n{con_matrix4}")
+
+    print("\nNow, showing the experiment results. See README.md for desired output with comments.")
+
+    con_matrix0_N = con_matrix0[0]
+    con_matrix0_P = con_matrix0[1]
+
+    con_matrix1_N = con_matrix1[0]
+    con_matrix1_P = con_matrix1[1]
+
+    con_matrix2_N = con_matrix2[0]
+    con_matrix2_P = con_matrix2[1]
+
+    con_matrix3_N = con_matrix3[0]
+    con_matrix3_P = con_matrix3[1]
+
+    con_matrix4_N = con_matrix4[0]
+    con_matrix4_P = con_matrix4[1]
+
+    print("Experiment | Accuracy           | Confusion Matrix ")
+    print(f"Baseline   | {score0} | [{con_matrix0_N} {con_matrix0_P}]")
+    print(f"Solution 1 | {score1} | [{con_matrix1_N} {con_matrix1_P}]")
+    print(f"Solution 2 | {score2} | [{con_matrix2_N} {con_matrix2_P}]")
+    print(f"Solution 3 | {score3} | [{con_matrix3_N} {con_matrix3_P}]")
+    print(f"Solution 4 | {score4} | [{con_matrix4_N} {con_matrix4_P}]")
+
+
+    
     
